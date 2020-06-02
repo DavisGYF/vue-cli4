74ffc0bd5f7179ad023d61da1a8044d2b1310e07
Davis <>
2020-06-02 14:12:53 +0800

    增加 vuex

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import create from "./utils/create";
 import Notice from "./components/kform/notice";
+import store from './store'
 
 Vue.config.productionTip = false;
 Vue.prototype.$notice = function(props) {
@@ -9,5 +10,6 @@ Vue.prototype.$notice = function(props) {
 };
 
 new Vue({
-  render: h => h(App),
+  store,
+  render: h => h(App)
 }).$mount('#app')
